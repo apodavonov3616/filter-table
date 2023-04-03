66c0903d40b416bf6be63b629bf797c68d8c1e63
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2023-04-03 13:33:36 -0400

    filter

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,46 +3,107 @@ import info from "./../../data/data"
 import { useEffect, useState } from "react";
 import "./Table.css"
 
-function Table(props) {
+function Table() {
 
-    const [data, setData] = useState([])
+    const [fullData, setFullData] = useState([])
+    const [currentData, setCurrentData] = useState([])
+    const [currentRegion, setCurrentRegion] = useState("all")
+    const [currentModel, setCurrentModel] = useState("all")
 
     useEffect(() => {
-        setData(info)
+        setFullData(info)
+        setCurrentData(info)
     }, [])
 
-    console.log(data)
+    const handleRegionChange = (e) => {
+        const selection = e.target.value;
+        setCurrentRegion(selection)
+        let filterByModel = modelFilter(fullData, currentModel)
+        let filterByRegion = regionFilter(filterByModel, selection)
+        setCurrentData(filterByRegion)
+    }
+
+    const handleModelChange = (e) => {
+        const selection = e.target.value;
+        setCurrentModel(selection)
+        let filterByRegion = regionFilter(fullData, currentRegion)
+        let filterByModel = modelFilter(filterByRegion, selection)
+        setCurrentData(filterByModel)
+
+    }
+
+    const regionFilter = (theData, currentRegion) => {
+        if (currentRegion === "all") {
+            return [...theData];
+        } else {
+            return theData.filter((datum) => {
+                return datum.region === currentRegion
+            })
+        }
+    }
+
+    const modelFilter = (theData, currentModel) => {
+        if (currentModel === "all") {
+            return [...theData];
+        } else {
+            return theData.filter((datum) => {
+                return datum.model === currentModel
+            })
+        }
+    }
+
     return (
-        <table>
-            <tr>
-                <th>
-                    region
-                </th>
-                <th>
-                    model
-                </th>
-                <th>
-                    sales
-                </th>
-            </tr>
+        <React.Fragment>
+            <div className="filters">
 
-            {data.map((car) => {
-                return (
-                    <tr>
-                        <td>
-                            {car.region}
-                        </td>
-                        <td>
-                            {car.model}
-                        </td>
-                        <td>
-                            {car.sales}
-                        </td>
-                    </tr>
-                )
-            })}
+                <label>region filter</label>
+                <select onChange={handleRegionChange}>
+                    {['all', 'US', 'EU', 'CA'].map((region) => {
+                        return (<option>
+                            {region}
+                        </option>)
+                    })}
+                </select>
+                <label>model filter</label>
+                <select onChange={handleModelChange}>
+                    {['all', 'A', 'B', 'C', 'D'].map((region) => {
+                        return (<option>
+                            {region}
+                        </option>)
+                    })}
+                </select>
+            </div>
+            <table>
+                <tr>
+                    <th>
+                        region
+                    </th>
+                    <th>
+                        model
+                    </th>
+                    <th>
+                        sales
+                    </th>
+                </tr>
 
-        </table>
+                {currentData.map((car) => {
+                    return (
+                        <tr>
+                            <td>
+                                {car.region}
+                            </td>
+                            <td>
+                                {car.model}
+                            </td>
+                            <td>
+                                {car.sales}
+                            </td>
+                        </tr>
+                    )
+                })}
+
+            </table>
+        </React.Fragment >
     )
 }
 
